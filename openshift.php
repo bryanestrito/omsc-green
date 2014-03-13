cbd6d35caab6e36ba4f6352b66e254019162300d
Bryan Estrito <bryan@klab.com>
2014-03-13 13:54:44 +0800

    Added openshift database connection

---
+++ openshift.php
@@ -0,0 +1,13 @@
+MySQL 5.5 database added.  Please make note of these credentials:
+
+    Root User: admin9IWmfQh
+    Root Password: Cj_qAfMX39Gs
+    Database Name: omscgreen
+
+Connection URL: mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/
+
+$OPENSHIFT_MYSQL_DB_HOST = 127.7.30.130
+$OPENSHIFT_MYSQL_DB_PORT = 3306
+
+You can manage your new MySQL database by also embedding phpmyadmin.
+The phpmyadmin username and password will be the same as the MySQL credentials above.
